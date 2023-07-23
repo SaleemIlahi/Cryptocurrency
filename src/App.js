74d9a59a813c209a683be3b08675aeb53fcdc569
Mohamed Saleem <saleemilahi21@gmail.com>
2023-07-23 16:59:18 +0530

    Completed silder component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Menubar from "./Components/Menubar/Menubar";
+import Slider from "./Components/Slider/Slider";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="left_cnt">
+        <Menubar />
+      </div>
+      <div className="right_cnt">
+        <Slider />
+      </div>
+    </div>
+  );
 }
 
 export default App;
